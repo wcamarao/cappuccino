529b56f7fa6271e3055a19b9d41f8fa39d08f63c
Wagner Montalvao Camarao <functioncallback@gmail.com>
2011-10-05 19:09:39 -0300

    draft for any() matcher

--- test/mock.test.js
+++ test/mock.test.js
@@ -2,3 +2,12 @@ var it = module.exports;
 var should = require('should');
 var mockjs = require('../lib/mock');
 
+var mock = mockjs.mock;
+
+it['should mock only methods of concrete objects'] = function() {
+  
+  var mockedLog = mock({ entry: 'log entry', get: function() { return this.entry; } });
+  mockedLog.should.not.have.property('entry');
+  mockedLog.should.have.property('get');
+  mockedLog.get.should.be.a('function');
+};
