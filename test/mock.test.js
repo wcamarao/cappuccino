d0d8c6902434d95af43aba73987cebdf02115e24
Wagner Montalvao Camarao <functioncallback@gmail.com>
2011-10-05 22:29:48 -0300

    improved matcher api

--- test/mock.test.js
+++ test/mock.test.js
@@ -1,13 +1,35 @@
 var it = module.exports;
 var should = require('should');
 var mockjs = require('../lib/mock');
-
 var mock = mockjs.mock;
 
-it['should mock only methods of concrete objects'] = function() {
+it['should mock only methods but not other types of properties'] = function() {
   
-  var mockedLog = mock({ entry: 'log entry', get: function() { return this.entry; } });
-  mockedLog.should.not.have.property('entry');
-  mockedLog.should.have.property('get');
-  mockedLog.get.should.be.a('function');
+  var mockedObject = mock(sampleObject());
+  
+  mockedObject.should.not.have.property('id');
+  mockedObject.should.not.have.property('array');
+  mockedObject.should.not.have.property('object');
+  mockedObject.should.not.have.property('bool');
+  mockedObject.should.not.have.property('nill');
+  mockedObject.should.not.have.property('undef');
+  mockedObject.should.not.have.property('date');
+  mockedObject.should.not.have.property('entry');
+  
+  mockedObject.should.have.property('get');
+  mockedObject.get.should.be.a('function');
 };
+
+function sampleObject() {
+  return {
+    id: 1,
+    array: [],
+    object: {},
+    bool: true,
+    nill: null,
+    undef: undefined,
+    date: new Date(),
+    entry: 'some text',
+    get: function() { return 0; }
+  };
+}
